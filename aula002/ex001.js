39a867633855d3d43834f06ad929868d108d4de9
Luan Batista <69101674+LuanJSX@users.noreply.github.com>
2025-06-27 13:52:17 -0300

    Aprendendo Condição

---
+++ aula002/ex001.js
@@ -0,0 +1,11 @@
+var vel = 58;
+
+console.log(`A velocidade do carro é ${vel} km/h`);
+if (vel > 60 ) {
+     console.log (``)
+     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+     console.log(`A velocidade atual do carro é ${vel} km/h`)
+     console.log(`A velocidade permitida é 60 km/h`)
+
+}
+console.log(`Dirija com segurança!`);
---
+++ aula002/ex002.js
@@ -0,0 +1,7 @@
+ var pais = "Brasil";
+
+ if (pais == "Brasil") {
+    console.log(`${pais} é um país do continente americano`);
+ }else {
+    console.log(`${pais} não e um pais do continente americano`);
+ }
